9ac9b67189bf74bf4ab7746bfd2a57d807fc90a1
Feroze Shahpurwala <sferoze@gmail.com>
2013-07-18 01:45:02 +0000

    Adding rest of files

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var homepage = fs.readFileSync("index.html", "utf-8");
 response.send(homepage.toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
