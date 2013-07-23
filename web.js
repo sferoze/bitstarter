8f26cccbe1ae8af662fda8a6c5a192c671717e33
Feroze Shahpurwala <sferoze@gmail.com>
2013-07-23 04:29:34 +0000

    web.js updated to use static images folder

--- web.js
+++ web.js
@@ -8,6 +8,7 @@ var homepage = fs.readFileSync("index.html", "utf-8");
 response.send(homepage.toString('utf8'));
 });
 
+app.use(express.static(images));
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
